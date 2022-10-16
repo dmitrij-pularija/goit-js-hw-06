7ab237ab7c9bb4e5e8b420eff0cb794c6169bdaa
dmitrij-pularija <dmitrij.pularija@gmail.com>
2022-10-16 12:56:13 +0300

    Update task-10.js
    random color

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,5 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${(Math.random().toString(16) + '000000').substring(2,8).toUpperCase()}`;
 }
 
 let box = document.querySelector("#boxes");
